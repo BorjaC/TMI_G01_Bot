1f95b86148869a319c0105b7cce163c4a6366e49
aserranob <alfonsosb@gmail.com>
2018-05-20 18:19:06 +0200

    Imagen

--- src/main/java/jvs/spring/picmicro/BotTelegram/Esteganografia.java
+++ src/main/java/jvs/spring/picmicro/BotTelegram/Esteganografia.java
@@ -1,19 +1,24 @@
 package jvs.spring.picmicro.BotTelegram;
 
 
+import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.sql.RowIdLifetime;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
+import javax.imageio.ImageIO;
+
+import org.telegram.telegrambots.api.methods.GetFile;
+import org.telegram.telegrambots.api.methods.send.SendDocument;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.PhotoSize;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardRemove;
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -55,8 +60,7 @@ public class Esteganografia extends TelegramLongPollingBot{
     }
 
 */	
-	
-	
+        
 	 public void onUpdateReceived (Update update) {
 		
 		 
@@ -139,39 +143,56 @@ public class Esteganografia extends TelegramLongPollingBot{
 	            // Message contains photo
 	            // Set variables
 	            long chat_id = update.getMessage().getChatId();
-
-	            //List<PhotoSize> photos = update.getMessage().getPhoto();
 	            photos = update.getMessage().getPhoto();
-	            image = (BufferedImage) photos;
+	            String sPath = getFilePath(photos.get(0));
+	          
+	            downloadPhotoByFilePath(sPath);
+	           
 	            
+	            //AQUÍ ESTEGANOGRAFIA
+	            
+	        
+                String foto = "C:\\Users\\alfon\\Pictures\\Camera Roll\\img1.jpg";
+                java.io.File fotofile = new java.io.File(foto);
+                try {
+					sendDocUploadingAFile(chat_id,fotofile,"");
+				} catch (TelegramApiException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+                
+                
+	            /*
+	          
 	            String f_id = photos.stream()
-	                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
 	                    .findFirst()
 	                    .orElse(null).getFileId();
 	            int f_width = photos.stream()
-	                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
 	                    .findFirst()
 	                    .orElse(null).getWidth();
 	            int f_height = photos.stream()
-	                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
 	                    .findFirst()
 	                    .orElse(null).getHeight();
+	          
 	            String caption = "file_id: " + f_id + "\nwidth: " + Integer.toString(f_width) + "\nheight: " + Integer.toString(f_height);
 	            SendPhoto msg = new SendPhoto()
 	                    .setChatId(chat_id)
 	                    .setPhoto(f_id)
 	                    .setCaption(caption);
+	   
 	            try {
 	                sendPhoto(msg); // Call method to send the message
 	            } catch (TelegramApiException e) {
 	                e.printStackTrace();
 	            }
+	            */
 	        }
 	    }
 	 
 	 
 	
 	
+	 
     //@Override
     public String getBotUsername() {
         return "TMIg1_bot";
@@ -180,10 +201,52 @@ public class Esteganografia extends TelegramLongPollingBot{
     @Override
     public String getBotToken() {
     	//TMI 5091...
-    	return "509183591:AAHdwgGIIAxEorn_2JHIzahbi9GvO4-s8L8";
+    	//return "509183591:AAHdwgGIIAxEorn_2JHIzahbi9GvO4-s8L8";
     	
     	//DASI
-    	//return "563754268:AAFBjVSFsJYdJai8LVBp4BGIJ7hxtDG80Y8";
+    	return "563754268:AAFBjVSFsJYdJai8LVBp4BGIJ7hxtDG80Y8";
     }
+    
+    
+    
+    //***************************revisar ABAJO https://github.com/rubenlagus/TelegramBots/wiki/FAQ
+    
+    
+    private void sendDocUploadingAFile(Long chatId, java.io.File save,String caption) throws TelegramApiException {
+
+        SendDocument sendDocumentRequest = new SendDocument();
+        sendDocumentRequest.setChatId(chatId);
+        sendDocumentRequest.setNewDocument(save);
+        sendDocumentRequest.setCaption(caption);
+        sendDocument(sendDocumentRequest);
+    }
+   
+    
+    
+    //descargar foto
+    
+    public java.io.File downloadPhotoByFilePath(String filePath) {
+        try {
+            // Download the file calling AbsSender::downloadFile method
+            return downloadFile(filePath);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    
+    
+    public String getFilePath(PhotoSize photo) {
+        Objects.requireNonNull(photo);
+
+        if (photo.hasFilePath()) { // If the file_path is already present, we are done!
+            return photo.getFilePath();
+        } else {
+        	 return null; // Just in case 	
+        }
+        
+    }
+    
 }
 
